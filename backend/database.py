c7a03b3e181b6ba20527777b7efc7bd50d9dfdf4
Ayaan <ayaanrk04@gmail.com>
2025-04-27 22:25:16 -0500

    Delete backend directory

--- backend/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-engine = create_engine('mysql+pymysql://root:kdotunicorn@localhost/sports2youdb', echo=True)
-
-Base = declarative_base()
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
